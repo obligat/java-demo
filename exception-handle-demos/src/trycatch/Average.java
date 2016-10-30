f5584b9df97c2607ab4572699504131028351750
dujinqiao <duzi1996@qq.com>
2016-10-30 23:08:59 +0800

    add try-catch in origin

--- exception-handle-demos/src/trycatch/Average.java
+++ exception-handle-demos/src/trycatch/Average.java
@@ -1,21 +1,25 @@
 package trycatch;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Average {
 
     public static void main(String[] args) {
-        Scanner console = new Scanner(System.in);
-        double sum = 0;
-        int count = 0;
-        while (true) {
-            int number = console.nextInt();
-            if (number == 0) {
-                break;
+        try {
+            Scanner console = new Scanner(System.in);
+            double sum = 0;
+            int count = 0;
+            while (true) {
+                int number = console.nextInt();
+                if (number == 0) {
+                    break;
+                }
+                sum += number;
+                count++;
             }
-            sum += number;
-            count++;
+            System.out.printf("average is  %.2f %n", sum / count);
+        } catch (InputMismatchException ex) {
+            System.out.println("Please input integer.");
         }
-        System.out.printf("average is  %.2f %n", sum / count);
     }
 }
