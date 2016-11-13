ef42b19545635201d61dbf6b70f675866320f108
dujinqiao <duzi1996@qq.com>
2016-11-13 23:26:36 +0800

    achaive nextInt function

--- exception-handle-demos/src/trycatch/Average.java
+++ exception-handle-demos/src/trycatch/Average.java
@@ -1,37 +1,34 @@
 package trycatch;
 
-import java.io.IOException;
-import java.util.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Scanner;
 
 public class Average {
 
     public static void main(String[] args) {
-        Scanner console = new Scanner(System.in);
         double sum = 0;
         int count = 0;
 
-        
-        try {
-            int ch = System.in.read();
-        } catch (java.io.IOException ex) {
-            ex.printStackTrace();
-        }
-        
-        try {
-            while (true) {
-                int number = console.nextInt();
-                if (number == 0) {
-                    break;
-                }
-                sum += number;
-                count++;
+        while (true) {
+            int number = nextInt();
+            if (number == 0) {
+                break;
             }
-
-        } catch (InputMismatchException ex) {
-            System.out.printf("Ignore illegal input : %s %n", console.next());
+            sum += number;
+            count++;
         }
-        System.out.printf("average is  %.2f %n", sum / count);
+        System.out.printf("average is  : %s %n", sum/count);
+    }
+    
+    static Scanner console = new Scanner(System.in);
+    
+    static int nextInt(){
+        String input = console.next();
+        while(!input.matches("\\d*")){
+        System.out.println("please input number");
+        input = console.next();
+        }
+        
+        return Integer.parseInt(input);
+    
     }
 }
