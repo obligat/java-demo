6e646cc1306959202065d1b684a93447311cbecc
dujinqiao <duzi1996@qq.com>
2016-11-15 22:42:05 +0800

    Try-With-Resources

--- exception-handle-demos/src/tryCatchAndThrows/FileUtil.java
+++ exception-handle-demos/src/tryCatchAndThrows/FileUtil.java
@@ -4,19 +4,13 @@ import java.io.*;
 import java.util.Scanner;
 
 public class FileUtil {
-    
+
     public static String readFile(String name) throws FileNotFoundException {
         StringBuilder text = new StringBuilder();
-        Scanner console = null;
-        try {
-            console = new Scanner(new FileInputStream(name));
+        try (Scanner console = new Scanner(new FileInputStream(name))) {
             while (console.hasNext()) {
                 text.append(console.nextLine())
                         .append("\n");
-            }
-        } finally {
-            if (console != null) {
-                console.close();
             }
         }
         return text.toString();
