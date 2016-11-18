a9c34c9806ea7be99ff34efdd32bae9f6a4a6d96
dujinqiao <duzi1996@qq.com>
2016-11-18 23:42:08 +0800

    add set--Students.java

---
+++ collection-map-demos/src/set/Students.java
@@ -0,0 +1,33 @@
+package set;
+
+import java.util.*;
+
+class Student {
+
+    private String name;
+    private String number;
+
+    Student(String name, String number) {
+        this.name = name;
+        this.number = number;
+
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%s ,%s)", name, number);
+    }
+
+}
+
+public class Students {
+
+    public static void main(String[] args) {
+        Set students = new HashSet();
+        students.add(new Student("joy", "D1212"));
+        students.add(new Student("lice", "D3312"));
+        students.add(new Student("dd", "D5334"));
+        
+        System.out.println(students);
+    }
+}
