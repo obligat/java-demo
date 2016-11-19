d6c749d637b6b24b847678188bb1c32dcedd5de2
dujinqiao <duzi1996@qq.com>
2016-11-19 20:29:00 +0800

    update Iterable -- ForEach.java with JDK8 programmer

--- collection-map-demos/src/interable/ForEach.java
+++ collection-map-demos/src/interable/ForEach.java
@@ -1,21 +1,25 @@
 package interable;
 
 import java.util.*;
+import static java.lang.System.out;
 
 public class ForEach {
 
     public static void main(String[] args) {
-        List names = Arrays.asList("Justion", "Kotton", "Moods");
-        forEach(names);
-        forEach(new HashSet(names));
-        forEach(new ArrayDeque(names));
+        List<String> names = Arrays.asList("Justion", "Kotton", "Moods");
+        
+//        JDK8 , Iterable 新增了 forEach() 方法
+
+        names.forEach(name->out.println(name));
+        new HashSet(names).forEach(name->out.println(name));
+        new ArrayDeque(names).forEach(name->out.println(name));
     }
 
-    static void forEach(Iterable iterable) {
-        for (Object o : iterable) {
-            System.out.println(o);
-        }
-    }
+//    static void forEach(Iterable iterable) {
+//        for (Object o : iterable) {
+//            System.out.println(o);
+//        }
+//    }
 }
 
 // result:
@@ -28,7 +32,6 @@ public class ForEach {
 //Justion
 //Kotton
 //Moods
-
 // 解析： 使用了 java.util.Arrays 的 static 方法 asList(), 这个方法接受不定长度自变量，
 // 可将指定的自变量收集为 List，List 是一种 Iterable ，所以可以使用 forEach() 方法。
 // HashSet 具有接受 Collection 的构造函数，List 是一种 Collection ，所以可以用来创建 HashSet，
