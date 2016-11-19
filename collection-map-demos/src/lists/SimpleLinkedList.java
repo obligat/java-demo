667d78a7f96b7961c71424e869cfdb6360e080e4
dujinqiao <duzi1996@qq.com>
2016-11-19 17:58:20 +0800

    add annotation in SimpleLinkedList.java

--- collection-map-demos/src/lists/SimpleLinkedList.java
+++ collection-map-demos/src/lists/SimpleLinkedList.java
@@ -2,20 +2,26 @@ package lists;
 
 public class SimpleLinkedList {
 
+    
+    // 将收集的对象用 Node 封装
+    
     private class Node {
 
         private Object elem;
 
         Node(Object o) {
             this.o = o;
-
         }
         Object o;
         Node next;
     }
 
+    // 第一个节点
+    
     private Node first;
 
+    // 新增 Node 封装对象，并由上一个 Node 的 next 参考
+    
     public void add(Object elem) {
         Node node = new Node(elem);
         if (first == null) {
@@ -34,6 +40,8 @@ public class SimpleLinkedList {
         last.next = node;
     }
 
+    // 访问所有 Node 并计数以取得长度
+    
     public int size() {
         int count = 0;
         Node last = first;
@@ -58,6 +66,9 @@ public class SimpleLinkedList {
         }
     }
 
+    
+    // 访问所有的 Node 并计数以取得对应的索引对象
+    
     private Object findElemOf(int index) {
         int count = 0;
         Node last = first;
