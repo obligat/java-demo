20475955f5a67a3497c36fa13989b65cce732111
dujinqiao <duzi1996@qq.com>
2016-11-19 21:30:41 +0800

    update Sort4

--- collection-map-demos/src/comparable/Sort4.java
+++ collection-map-demos/src/comparable/Sort4.java
@@ -2,19 +2,11 @@ package comparable;
 
 import java.util.*;
 
-class StringComparator implements Comparator<String> {
-
-    @Override
-    public int compare(String s1, String s2) {
-        return -s1.compareTo(s2);
-    }
-}
-
 public class Sort4 {
 
     public static void main(String[] args) {
         List<String> words = Arrays.asList("d", "g", "w", "a", "l");
-        Collections.sort(words, new StringComparator());
+        Collections.sort(words, (s1, s2) -> -s1.compareTo(s2));
         System.out.println(words);
     }
 }
