10b5067264d002db2ab396231fe1bf460d914d87
dujinqiao <duzi1996@qq.com>
2016-11-19 22:01:12 +0800

    add treeMap reverse result

--- collection-map-demos/src/map/treeMap/MessagesReverse.java
+++ collection-map-demos/src/map/treeMap/MessagesReverse.java
@@ -1,20 +1,20 @@
-
 package map.treeMap;
 
 import java.util.*;
 import static java.lang.System.out;
 
-public class Messages {
-
+public class MessagesReverse {
+    
     public static void main(String[] args) {
-        Map<String, String> messages = new TreeMap();
+        Map<String, String> messages = new TreeMap<>((s1, s2) -> -s1.compareTo(s2));
         messages.put("Justin", "Hello ! Justin's message.");
         messages.put("Monica", "Hello ! Monica's message.");
         messages.put("Irene", "Hello ! Irene's message.");
-
+        
         out.println(messages);
-
+        
     }
 }
 
-// {Irene=Hello ! Irene's message., Justin=Hello ! Justin's message., Monica=Hello ! Monica's message.}
+
+// {Monica=Hello ! Monica's message., Justin=Hello ! Justin's message., Irene=Hello ! Irene's message.}
