acd971f950cb338f1721c42bd52c8530ba5db3ff
dujinqiao <duzi1996@qq.com>
2016-11-19 21:31:45 +0800

    update Sort4

--- collection-map-demos/src/comparable/Sort4.java
+++ collection-map-demos/src/comparable/Sort4.java
@@ -6,7 +6,7 @@ public class Sort4 {
 
     public static void main(String[] args) {
         List<String> words = Arrays.asList("d", "g", "w", "a", "l");
-        Collections.sort(words, (s1, s2) -> -s1.compareTo(s2));
+        words.sort((s1, s2) -> -s1.compareTo(s2));
         System.out.println(words);
     }
 }
