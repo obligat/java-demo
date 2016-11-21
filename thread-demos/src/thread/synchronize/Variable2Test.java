a283d1714af89232068821c976a0790c5f50d8b0
dujinqiao <duzi1996@qq.com>
2016-11-21 20:43:05 +0800

    add Variable2Test.java

---
+++ thread-demos/src/thread/synchronize/Variable2Test.java
@@ -0,0 +1,45 @@
+package thread.synchronize;
+
+class Variable2 {
+
+    static int i = 0, j = 0;
+
+    static synchronized void one() {
+        i++;
+        j++;
+    }
+
+    static synchronized void two() {
+        System.out.printf("i = %d, j = %d %n", i, j);
+    }
+}
+
+public class Variable2Test {
+
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(() -> {
+            while (true) {
+                Variable2.one();
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            while (true) {
+                Variable2.two();
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+    }
+}
+
+//...
+//i = 97676, j = 97676 
+//i = 97676, j = 97676 
+//i = 97676, j = 97676 
+//i = 97676, j = 97676 
+//i = 97676, j = 97676 
+//i = 97676, j = 97676 
+//i = 97676, j = 97676 
+//...
