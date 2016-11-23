64891a211c340cdbe67c2ff8cd0b2673f5e038d7
dujinqiao <duzi1996@qq.com>
2016-11-23 11:40:47 +0800

    update StringOrderDemo use anther write way

--- lambda-demos/src/StringOrderDemo.java
+++ lambda-demos/src/StringOrderDemo.java
@@ -5,7 +5,7 @@ public class StringOrderDemo {
 
     public static void main(String[] args) {
         String[] names = {"Justin", "caterpillar", "Bush"};
-        Arrays.sort(names, StringOrder::byLexicographyIgnoreCase);
+        Arrays.sort(names, String::compareToIgnoreCase);
         System.out.println(Arrays.toString(names));
     }
 }
