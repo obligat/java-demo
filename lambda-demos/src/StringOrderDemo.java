c503dac4995f9d29f834cdfdb0795012fc665961
dujinqiao <duzi1996@qq.com>
2016-11-23 11:32:31 +0800

    update StringOrderDemo.java use Method Reference in JDK8

--- lambda-demos/src/StringOrderDemo.java
+++ lambda-demos/src/StringOrderDemo.java
@@ -5,7 +5,7 @@ public class StringOrderDemo {
 
     public static void main(String[] args) {
         String[] names = {"Justin", "caterpillar", "Bush"};
-        Arrays.sort(names, (s1,s2)->StringOrder.byLength(s1, s2));
+        Arrays.sort(names, StringOrder::byLength);
         System.out.println(Arrays.toString(names));
     }
 }
