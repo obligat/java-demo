e37c31730043512dd8b71a4ccecec786ef7f396c
dujinqiao <duzi1996@qq.com>
2016-11-23 11:21:36 +0800

    add lambda-demos

---
+++ lambda-demos/src/LambdaDemo.java
@@ -0,0 +1,17 @@
+
+import java.util.Arrays;
+
+public class LambdaDemo {
+    
+    public static void main(String[] args) {
+        String[] names = {"Justin", "caterpillar", "Bush"};
+        Arrays.sort(names, (name1, name2) -> name1.length() - name2.length());
+        System.out.println(Arrays.toString(names));
+    }
+}
+
+
+/*
+run:
+[Bush, Justin, caterpillar]
+*/
