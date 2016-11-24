96fb4cab2413b25c8c805a6eed6d4c1300d1f1e6
dujinqiao <duzi1996@qq.com>
2016-11-24 10:26:52 +0800

    add EmployeeDemo2.java

---
+++ lambda-demos/src/stream/EmployeeDemo2.java
@@ -0,0 +1,76 @@
+package stream;
+
+import static java.lang.System.out;
+import java.util.Arrays;
+import java.util.List;
+
+public class EmployeeDemo2 {
+    
+    public static void main(String[] args) {
+        List<Employee2> employees = Arrays.asList(
+                new Employee2("Tom", 32, Gender2.MALE),
+                new Employee2("Lisa", 34, Gender2.FEMALE),
+                new Employee2("Edion", 12, Gender2.MALE)
+        );
+        
+        int sum = employees.stream()
+                .filter(e -> e.getGender() == Gender2.MALE)
+                .mapToInt(Employee2::getAge)
+                .reduce((total, age) -> total + age)
+                .getAsInt();
+        
+        long males = employees.stream()
+                .filter(e -> e.getGender() == Gender2.MALE)
+                .count();
+        
+        long average = employees.stream()
+                .filter(e -> e.getGender() == Gender2.MALE)
+                .mapToInt(Employee2::getAge)
+                .reduce((total, age) -> total + age)
+                .getAsInt() / males;
+        
+        int max = employees.stream()
+                .filter(e -> e.getGender() == Gender2.MALE)
+                .mapToInt(Employee2::getAge)
+                .reduce(0, (currMax, age) -> age > currMax ? age : currMax);
+        
+        Arrays.asList(sum, average, max).forEach(out::println);
+    }
+}
+
+enum Gender2 {
+    FEMALE, MALE
+}
+
+class Employee2 {
+    
+    private String name;
+    private Integer age;
+    private Gender2 gender;
+    
+    public Employee2(String name, Integer age, Gender2 gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public Integer getAge() {
+        return age;
+    }
+    
+    public Gender2 getGender() {
+        return gender;
+    }
+}
+
+
+/*
+run:
+44
+22
+32
+*/
