bcc69179c263e40b6680daf929b1f35a80c97fd3
dujinqiao <duzi1996@qq.com>
2016-11-27 16:36:21 +0800

    add ConnectionDemo.java

--- jdbc-demos/src/connection/ConnectionDemo.java
+++ jdbc-demos/src/connection/ConnectionDemo.java
@@ -1,6 +1,17 @@
-
 package connection;
+
+import java.sql.*;
+import static java.lang.System.out;
 
 public class ConnectionDemo {
 
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        String jdbcUrl = "jdbc:mysql://localhost:3306/abc";
+        String user = "root";
+        String passwd = "dujinqiao";
+        try (Connection conn = DriverManager.getConnection(jdbcUrl, user, passwd)) {
+            out.printf("already %s database connection", conn.isClosed() ? "closed" : "open");
+        }
+    }
 }
